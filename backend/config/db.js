078a940cdf39a1476f9ecd9b227a44ad5b672307
prashant-Vss <prashant.mishra@vishleshan.net>
2025-12-09 10:24:06 +0530

    Initial commit

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+// backend/config/db.js
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log("DB Connection Error:", err));
+
+module.exports = sequelize;
